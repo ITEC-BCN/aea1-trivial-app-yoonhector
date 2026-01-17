5be39de6327cc338b8eb12b18e76bc155560cbd2
Yoon <yoon.lopez.7e9@itb.cat>
2026-01-17 15:55:43 +0100

    Screen navigation controllers done

--- app/src/main/java/com/example/trivialapp_base/MainActivity.kt
+++ app/src/main/java/com/example/trivialapp_base/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -12,13 +13,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.trivialapp_base.ui.theme.TrivialAPP_BaseTheme
+import com.example.trivialapp_base.view.GameScreen
 import com.example.trivialapp_base.view.MenuScreen
 import com.example.trivialapp_base.view.ResultScreen
+import com.example.trivialapp_base.viewmodel.GameViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,23 +34,19 @@ class MainActivity : ComponentActivity() {
                 // Controlador de navegación
                 val navigationController = rememberNavController()
 
-                NavHost(
-                    navController = navigationController as NavHostController,
-                    startDestination = Routes.Menu.route
-                ) {
-                    composable(Routes.Menu.route) { MenuScreen(navigationController, easy) }
-                    composable(Routes.Game.route) { GameScreen(navigationController) }
-                    composable(Routes.Result.route) { ResultScreen(navigationController) }
-                }
-
                 // Instanciamos el ViewModel una vez
-
+                val gameViewModel: GameViewModel by viewModels()
 
 
                 // Definición de rutas y navegación
-
-
-
+                NavHost(
+                    navController = navigationController,
+                    startDestination = Routes.Menu.route
+                ) {
+                    composable(Routes.Menu.route) { MenuScreen(navigationController, gameViewModel ) }
+                    composable(Routes.Game.route) { GameScreen(navigationController, gameViewModel) }
+                    composable(Routes.Result.route) { ResultScreen(navigationController, gameViewModel) }
+                }
             }
         }
     }
