d60106c77eddf5b92cedf04857f1ef33f1f870a0
HectorPM28 <hector.pasantes.7e9@itb.cat>
2026-01-21 10:45:12 +0100

    Added GameScreen

--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -1,10 +1,61 @@
 package com.example.trivialapp_base.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun GameScreen(navController: NavController, viewModel: GameViewModel) {
+    var questionsLeft by rememberSaveable() { mutableStateOf(0.0f) }
+    var timeLeft by rememberSaveable() { mutableStateOf(1f) }
 
+    Column (horizontalAlignment = Alignment.CenterHorizontally){
+        Text("hola")
+        Text("hola")
+        Text("hola")
+        Text("hola")
+        Text("hola")
+        Text("hola")
+
+        Text("Texto que contiene pregunta")
+        Row {
+            Button(onClick = {questionsLeft += 0.1f
+                timeLeft -= 0.1f})
+            {
+                Text("Pregunta1")
+            }
+            Button(onClick = {questionsLeft += 0.1f
+                timeLeft = 1f})
+            {
+                Text("Pregunta2")
+
+            }
+        }
+        Row {
+            Button(onClick = {questionsLeft += 0.1f
+                timeLeft = 1f})
+            {
+                Text("Pregunta3")
+            }
+            Button(onClick = {questionsLeft += 0.1f
+                timeLeft = 1f})
+            {
+                Text("Pregunta4")
+            }
+        }
+        LinearProgressIndicator(progress = questionsLeft)
+    }
 }
