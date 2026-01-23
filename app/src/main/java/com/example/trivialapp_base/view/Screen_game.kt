396cb26e36275bff8fed7f35687db0709a87c00e
HectorPM28 <hector.pasantes.7e9@itb.cat>
2026-01-23 09:41:51 +0100

    Update Screen_game.kt

--- app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
+++ app/src/main/java/com/example/trivialapp_base/view/Screen_game.kt
@@ -1,6 +1,5 @@
 package com.example.trivialapp_base.view
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.width
@@ -8,10 +7,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -20,39 +15,55 @@ import com.example.trivialapp_base.viewmodel.GameViewModel
 
 @Composable
 fun GameScreen(navController: NavController, viewModel: GameViewModel) {
-    var questionsLeft by rememberSaveable() { mutableStateOf(0.0f) }
 
     Column (horizontalAlignment = Alignment.CenterHorizontally){
-        Text("hola")
-        Text("hola")
-        Text("hola")
-        Text("hola")
-        Text("hola")
-        Text("hola")
+        LinearProgressIndicator(progress = viewModel.tiempoRestante,
+            Modifier.width(400.dp))
 
-        Text("Texto que contiene pregunta")
+        Text(viewModel.preguntaActual!!.pregunta)
         Row {
-            Button(onClick = {questionsLeft += 0.1f })
+            Button(
+                onClick = {
+                    viewModel.progressBar += 0.1f
+                    viewModel.responderPregunta(viewModel.respuestasMezcladas[0])
+                }
+            )
             {
-                Text("Pregunta1")
+                Text(viewModel.respuestasMezcladas[0])
             }
-            Button(onClick = {questionsLeft += 0.1f })
+            Button(
+                onClick = {
+                    viewModel.progressBar += 0.1f
+                    viewModel.responderPregunta(viewModel.respuestasMezcladas[1])
+                }
+            )
             {
-                Text("Pregunta2")
+                Text(viewModel.respuestasMezcladas[1])
 
             }
         }
         Row {
-            Button(onClick = {questionsLeft += 0.1f })
+            Button(
+                onClick = {viewModel.progressBar += 0.1f
+                    viewModel.responderPregunta(viewModel.respuestasMezcladas[2])}
+            )
             {
-                Text("Pregunta3")
+                Text(viewModel.respuestasMezcladas[2])
             }
-            Button(onClick = {questionsLeft += 0.1f })
+            Button(
+                onClick = {viewModel.progressBar += 0.1f
+                    viewModel.responderPregunta(viewModel.respuestasMezcladas[3])}
+            )
             {
-                Text("Pregunta4")
+                Text(viewModel.respuestasMezcladas[3])
+            }
+
+            if (viewModel.juegoTerminado) {
+                navController.navigate("ResultScreen")
             }
         }
-        LinearProgressIndicator(progress = questionsLeft,
+        LinearProgressIndicator(progress = viewModel.progressBar,
             Modifier.width(400.dp))
     }
+
 }
