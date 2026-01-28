9b0132b4d1f70278c833238cd0f40c507a72190f
HectorPM28 <hector.pasantes.7e9@itb.cat>
2026-01-28 10:39:29 +0100

    Added colors

--- app/src/main/java/com/example/trivialapp_base/ui/theme/Color.kt
+++ app/src/main/java/com/example/trivialapp_base/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val SHINee = Color(0xFF88D8C0)
+val LightSHINee = Color(0xFFe3f8f2)
+val DarkSHINee = Color(0xFF52A186)
